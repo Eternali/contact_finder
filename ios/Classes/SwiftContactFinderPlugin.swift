3a0e10830cf79959d43e6193f7e02be7fc354bfb
Conrad Heidebrecht <eternali@v1022-rn-124-125.campus-dynamic.uwaterloo.ca>
2018-06-03 14:59:38 -0400

    working on ios part now

--- ios/Classes/SwiftContactFinderPlugin.swift
+++ ios/Classes/SwiftContactFinderPlugin.swift
@@ -1,8 +1,9 @@
 import Flutter
 import UIKit
+import ContactsUI
 
-static let channelName = "contact_finder"
-static let pickerCode = 85500
+let channelName = "contact_finder"
+let pickerCode = 85500
 
 public class SwiftContactFinderPlugin: NSObject, FlutterPlugin {
 
@@ -18,6 +19,49 @@ public class SwiftContactFinderPlugin: NSObject, FlutterPlugin {
       result({  })
     } else {
       result(FlutterMethodNotImplemented)
+    }
+  }
+
+  func selectContact(picker: CNContactPickerViewController, didSelectContact contact: CNContact) {
+    picker.dismissViewControllerAnimated(true, completion: nil)
+    if contact.phoneNumbers.count > 1 {
+      let multipleNumbersAlert = UIAlertController(
+        title: "Which one?",
+        message: "This contact has multiple phone numbers, which one do you want to use?",
+        preferredStyle: UIAlertControllerStyle.Alert
+      )
+      for number in contact.phoneNumbers {
+        if let actualNumber = number.value as? CNPhoneNumber {
+          var numberLabel = number.label
+
+          let actionTitle = numberLabel + " - " + actualNumber.stringVal
+          let numberAction = UIAlertAction(
+            title: actionTitle,
+            style: UIAlertActionStyle.Default,
+            handler: { (action) -> Void in
+              var nameToSave = contact.givenName == "" ? contact.familyName == "" ? "Unknown Name" : contact.familyName : contact.givenName
+              
+              if let imageData = contact.imageData {
+                let userImage = UIImage(data: imageData)
+              }
+              // use contact information
+              // actualNumber.stringVal
+            }
+          )
+          multipleNumbersAlert.addAction(numberAction)
+        }
+      }
+      let cancelAction = UIAlertAction(
+        title: "Cancel",
+        style: UIAlertActionStyle.Cancel,
+        handler: { (action) -> Void in }
+      )
+      multipleNumbersAlert.addAction(cancelAction)
+      self.presentViewController(multipleNumbersAlert, animated: true, completion: nil)
+    } else {
+      if contact.phoneNumbers.count > 0 {
+
+      }
     }
   }
 
